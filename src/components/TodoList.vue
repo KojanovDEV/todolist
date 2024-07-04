dfb0367143188ed00434665d030fcd89140dc865
Kkuralll <116596044+Kkuralll@users.noreply.github.com>
2024-07-04 12:03:42 +0500

    ToDo list uspeshno

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+    <body>
+    <div class="container">
+        <div id="newtask">
+        <input type="text" v-model="newTodo" placeholder="Yangi ishni joylashtirish ..." />
+        <button @click="addTodo()">Qo'shish</button>
+        <div id="tasks">
+            <ul>
+            <li class="task" v-for="(todo, index) in todos" :key="index">
+                {{ todo }}
+                <button @click="removeTodo(index)"><i class="far fa-trash-alt"></i></button>
+            </li>
+        </ul>
+        </div>
+        </div>
+    </div>
+    </body>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodo: '',
+            todos: []
+        }
+    },
+    methods: {
+        addTodo() {
+            if (!this.newTodo) return
+            this.todos.unshift(this.newTodo)
+            this.newTodo = ''
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1)
+        }
+    }
+}
+</script>
+
